0c23dfd7a154463ab7d20dc4e7f08621bdbfe2fa
Julian Köhler <julkoehl@icloud.com>
2023-02-27 17:48:25 +0100

    Added the similar tweet cards

---
+++ xAI_ShapleyV/classifier/src/similarTweets.js
@@ -0,0 +1,71 @@
+async function getData() {
+  const response = await fetch("./similarTweets.json");
+  const { data } = await response.json();
+  return data;
+}
+
+async function createTweetCards() {
+  const tweets = await getData();
+  const divContainer = document.getElementById("similar-tweets");
+
+  tweets.forEach(tweet => {
+    const container = document.createElement("div");
+    container.classList.add("similar-tweet");
+    container.classList.add("card");
+
+    const textID = document.createElement("h3");
+    textID.textContent = tweet.text_id;
+
+    const mainContent = document.createElement("div");
+    mainContent.classList.add("similar-tweet__main-content");
+    const scoreContainer = document.createElement("div");
+
+    const text = document.createElement("p");
+    text.textContent = tweet.text;
+
+    const cosSimContainer = document.createElement("div");
+    cosSimContainer.classList.add("score");
+    const cosSimHeading = document.createElement("h3");
+    const cosSimValueElement = document.createElement("p");
+    const cosSimValue = tweet.cos_sim * 100;
+    cosSimHeading.textContent = "Cosine-Similarity:";
+    cosSimValueElement.textContent = cosSimValue.toFixed(2) + " %";
+
+    const predictionScoreContainer = document.createElement("div");
+    predictionScoreContainer.classList.add("score");
+    const predictionHeading = document.createElement("h3");
+    const predictionValueElement = document.createElement("p");
+    const predictionValue = tweet.prediction_score * 100;
+    predictionHeading.textContent = "Prediction-Score:";
+    predictionValueElement.textContent = predictionValue.toFixed(2) + " %";
+
+    const label = document.createElement("div");
+
+    if (tweet.task_1 === "HOF") {
+      label.textContent = "Hate Speech";
+      label.classList.add("hatespeech-label");
+    } else {
+      label.textContent = "No Hate Speech";
+      label.classList.add("no-hatespeech-label");
+    }
+
+    cosSimContainer.appendChild(cosSimHeading);
+    cosSimContainer.appendChild(cosSimValueElement);
+
+    predictionScoreContainer.appendChild(predictionHeading);
+    predictionScoreContainer.appendChild(predictionValueElement);
+
+    container.appendChild(textID);
+    container.appendChild(mainContent);
+    mainContent.appendChild(text);
+    scoreContainer.appendChild(cosSimContainer);
+    scoreContainer.appendChild(predictionScoreContainer);
+    scoreContainer.appendChild(label);
+
+    mainContent.appendChild(scoreContainer);
+
+    divContainer.appendChild(container);
+  });
+}
+
+createTweetCards();
---
+++ xAI_ShapleyV/classifier/src/text-plot.js
@@ -0,0 +1,114 @@
+// Define the data
+var players = ["Trump", "sucks", "is", "he", "asshole", "great"];
+var values = [-0.234, -0.356, -0.243, 0.134, -0.548, 0.176];
+
+// Calculate the Shapley values
+var shapleyValues = values.sort((a, b) => a - b);
+
+// Define the chart element
+var chart = d3.select("#chart");
+
+// Define the group elements
+var groups = chart
+  .selectAll(".group")
+  .data(shapleyValues)
+  .enter()
+  .append("div")
+  .attr("class", "group")
+  .style("display", "inline-flex")
+  .style("flex-direction", "column")
+  .style("justify-content", "center");
+
+// Define the bar elements
+var bars = groups
+  .append("div")
+  .attr("class", "bar")
+  .style("width", function (d) {
+    return Math.abs(d) * 500 + "px";
+  })
+  .style("height", "35px")
+  .style("background-color", d => (d > 0 ? "rgba(70, 130, 180, 0.7" : "rgba(255, 0, 0, 0.7)"))
+  .style("display", "flex")
+  .style("justify-content", "center")
+  .style("align-items", "center");
+
+// Add a value element to each bar
+bars
+  .append("div")
+  .attr("class", "value")
+  .text(function (d) {
+    return d.toFixed(3);
+  });
+
+// Define the label elements
+var labels = groups
+  .append("div")
+  .attr("class", "label")
+  .text(function (d, i) {
+    return players[i];
+  })
+  .style("left", "-5%")
+  .style("padding", "0")
+  .style("margin-left", function (d) {
+    return Math.abs(d) * 200 + "px";
+  })
+  .style("margin-top", "1rem");
+
+// Define the tick function to update bar widths and label positions
+function updateBars() {
+  bars
+    .style("width", function (d) {
+      return Math.abs(d) * 200 + "px";
+    })
+    .style("background-color", function (d) {
+      return d > 0 ? "green" : "red";
+    });
+  labels.style("margin-left", function (d) {
+    return Math.abs(d) * 200 + "px";
+  });
+}
+
+// Function to calculate the Shapley values
+function calculateShapleyValues(values) {
+  var n = values.length;
+  var shapleyValues = new Array(n);
+
+  for (var i = 0; i < n; i++) {
+    var numerator = 0;
+    var denominator = factorial(n - 1);
+    for (var j = 0; j < 1 << (n - 1); j++) {
+      var coalition = [];
+      for (var k = 0; k < n - 1; k++) {
+        if ((j & (1 << k)) !== 0) {
+          coalition.push(k);
+        }
+      }
+      coalition.push(i);
+      coalition.sort();
+      var m = coalition.length;
+      var contribution = values[coalition[m - 1]] - (m > 1 ? values[coalition[m - 2]] : 0);
+      numerator += contribution * binomial(n - 1, m - 1);
+    }
+    shapleyValues[i] = numerator / denominator;
+  }
+
+  return shapleyValues;
+}
+
+// Function to calculate the binomial coefficient
+function binomial(n, k) {
+  var result = 1;
+  for (var i = 0; i < k; i++) {
+    result *= (n - i) / (i + 1);
+  }
+  return result;
+}
+
+// Function to calculate the factorial of a number
+function factorial(n) {
+  var result = 1;
+  for (var i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
